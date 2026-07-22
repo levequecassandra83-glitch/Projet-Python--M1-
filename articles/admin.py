92fafff3d8b2dc9720a21eeb0981c87ed2c37f63
ThomasCbls <camboulasthomas@gmail.com>
2026-07-22 13:51:19 +0200

    style

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+
 from .models import Article
 
-admin.site.register(Article)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'date', 'tags')
+    list_filter = ('date',)
+    search_fields = ('titre', 'contenu')
